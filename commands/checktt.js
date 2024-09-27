5cfd310da820cbc7ec298f8e2dc72c39b17bfb29
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-09-27 21:25:52 +0700

    Create checktt.js

---
+++ commands/checktt.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'checktt',
+  description: 'Kiểm tra số tin nhắn của user',
+  usage: '[nashPrefix]checktt',
+  nashPrefix: false,
+  execute: async (api, event, args, prefix) => {
